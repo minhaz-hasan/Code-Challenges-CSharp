83304f560033709b47d89496c9313b699371f09b
minhaz-hasan <shuvo067@gmail.com>
2014-04-22 00:25:18 +0600

    Added an abstract class for basic mechanism of printing the html characters and average reviews

---
+++ Code_Challenges_CSharp/HtmlContentHelper.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Code_Challenges_CSharp.Interfaces;
+
+namespace Code_Challenges_CSharp
+{
+    /// <summary>
+    /// This abstract class defines basic mechanism of printing the html characters and average reviews.
+    /// </summary>
+    public class HtmlContentHelper
+    {
+        private readonly IUrlProcessor _uriProcessor;
+        private readonly IHtmlContentProcessor _htmlContentProcessor;
+        private readonly IPrinter _printer;
+
+        protected HtmlContentHelper(IUrlProcessor uriProcessor, IHtmlContentProcessor htmlContentProcessor, IPrinter printer)
+        {
+            _printer = printer;
+            _htmlContentProcessor = htmlContentProcessor;
+            _uriProcessor = uriProcessor;
+        }
+
+        public virtual void ProcessAverageRating()
+        {
+            var averageRating = _htmlContentProcessor.GetReviews(_uriProcessor.ProcessUrlWithoutWhitespace());
+            _printer.Print("Average rating from the reviews on the first page of reviews", averageRating.Average().ToString("F1"));
+        }
+
+        public virtual void PrintNumberOfCharactersFromUrl()
+        {
+            var htmlString = _uriProcessor.HtmlData;
+            var htmlStringWithoutSpace = _uriProcessor.ProcessUrlWithoutWhitespace();
+
+            var listOfHtmlContents = new Dictionary<string, int>
+            {
+                {"Total number of chars with whitespace", htmlString.Length},
+                {"Total number of chars without whitespace", htmlStringWithoutSpace.Length}
+            };
+
+            foreach (var listOfHtmlContent in listOfHtmlContents)
+            {
+                _printer.Print(listOfHtmlContent.Key, listOfHtmlContent.Value.ToString());
+            }
+        }
+    }
+}
