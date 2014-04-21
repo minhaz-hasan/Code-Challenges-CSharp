4666bd3ce26472f1cd65c99a0f116e08c8899f86
minhaz-hasan <shuvo067@gmail.com>
2014-04-22 00:20:24 +0600

    Added the implementation of IPrinter

---
+++ Code_Challenges_CSharp/Services/PrinterService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Code_Challenges_CSharp.Interfaces;
+
+namespace Code_Challenges_CSharp.Services
+{
+    public class PrinterService : IPrinter
+    {
+        public void Print(string message, string data)
+        {
+            Console.WriteLine("{0}: {1}\n", message, data);
+        } 
+    }
+}
