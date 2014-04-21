a8584228089ee124fda73c55a586cf8a68f4bcf7
minhaz-hasan <shuvo067@gmail.com>
2014-04-22 00:14:35 +0600

    Added an interface for processing html content and returning the reviews

---
+++ Code_Challenges_CSharp/Interfaces/IHtmlContentProcessor.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace Code_Challenges_CSharp.Interfaces
+{
+    /// <summary>
+    /// IHtmlContentProcessor defines the actions for processing the html content and returns list of reviews from the page.
+    /// </summary>
+    public interface IHtmlContentProcessor
+    {
+        List<double> GetReviews(string html); // Renames it to GetReviews() from GetAverage()
+    }
+}
