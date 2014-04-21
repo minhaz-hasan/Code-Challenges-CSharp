03530e308e840cb2704e1b592fbf09fbbec26d75
minhaz-hasan <shuvo067@gmail.com>
2014-04-22 00:16:44 +0600

    Added the implementation of IHtmlContentProcessor

---
+++ Code_Challenges_CSharp/Services/HtmlContentProcessorService.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using Code_Challenges_CSharp.Interfaces;
+
+namespace Code_Challenges_CSharp.Services
+{
+    public class HtmlContentProcessorService : IHtmlContentProcessor
+    {
+
+        public int StartPos { get; private set; }
+        public int StartIdx { get; private set; }
+        public string SubStringValue { get; private set; }
+
+        public HtmlContentProcessorService(int startPos, int startIdx, string subStringValue)
+        {
+            StartPos = startPos;
+            StartIdx = startIdx;
+            SubStringValue = subStringValue;
+        }
+
+        public List<double> GetReviews(string html)
+        {
+            var extractListOfRatings = new List<double>();
+            while (StartPos < html.Length)
+            {
+                StartPos = html.IndexOf(SubStringValue, StartPos, StringComparison.Ordinal);
+
+                if (StartPos == -1)
+                {
+                    break;
+                }
+
+                var tempStr = html.Substring(StartPos + StartIdx, 1);
+                extractListOfRatings.Add(double.Parse(tempStr));
+                StartPos += StartIdx;
+            }
+            return extractListOfRatings;
+        }
+    }
+}
