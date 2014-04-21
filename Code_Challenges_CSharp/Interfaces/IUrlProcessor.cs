bd3dd74ee80456a074128813d0abdd66914b181f
minhaz-hasan <shuvo067@gmail.com>
2014-04-22 00:03:09 +0600

    Move the interface into a separate folder

---
+++ Code_Challenges_CSharp/Interfaces/IUrlProcessor.cs
@@ -0,0 +1,13 @@
+﻿namespace Code_Challenges_CSharp
+{
+    /// <summary>
+    /// IUrlProcessor defines the actions for retrieving html content and stores that content and process that content.</summary>
+    public interface IUrlProcessor
+    {
+        string HtmlData { get; } // Alternative of static variable I think and renamed it from _htmlContent to HtmlData.
+        string Url { get; }
+
+        string ProcessUrl();
+        string ProcessUrlWithoutWhitespace();
+    }
+}
